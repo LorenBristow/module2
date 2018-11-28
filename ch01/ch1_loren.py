85717ec78ffed2b0bbf7014944543c677e44db4d
Loren Bristow <lorendup@gmail.com>
2018-11-28 16:02:13 +0000

    new file

---
+++ ch01/ch1_loren.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 28 14:28:02 2018
+
+@author: 612383225
+"""
+
+A = 2+3
+B = 2.9+3.2
+C = (3.2/2.0)+7
+D = 2**3
+E = 4**(1/2)
+F = str(6)+'abacus'
+print (A)
+print (B)
+
+
+
+
