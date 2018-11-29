9c2202b1ab35dbd7efaad0b9cd326e581e21b0c0
Loren Bristow <lorendup@gmail.com>
2018-11-29 10:05:24 +0000

    chapter 2 file homework committed

---
+++ ch02/ch02_loren.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a = 1
+a = a+1
+print(a)
+b = 'hello'
+print(b)
+c = b.title()
+print(b)
+print(c)
+d = 'hello'
+e = d.title()
+print(d)
+print(e)
+name = 'Dave'
+f = 'Hello! {0}!'.format(name)
+print(f)
+name = 'Loren'
+f = 'Hello! {0}!'.format(name)
+print(f)
+
+
+age = 5
+age = 'almost there'
+age = 29.5
+age = 'i really don\'t know' 
+print(age)
+
+print(type(2))
+
+print('hello' + 'world')
+print("Joke" * 3)
+#print("Chen" + 3) Error because you can't put int and str together in concat. convert int to string.2 fixes below. 
+print("Chen" + "3")
+print("Chen" + str(3))
+print('hello'.upper())
+print('GOODBYE'.lower())
+print('the lord of the rings'.title())
