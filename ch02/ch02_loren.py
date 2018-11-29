9f5b425a3b51b8fc2e690936a94c10541ad2cd44
Loren Bristow <lorendup@gmail.com>
2018-11-29 11:37:28 +0000

    finally got ch2 file up, thanks IZA

--- ch02/ch02_loren.py
+++ ch02/ch02_loren.py
@@ -1,76 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Thu Nov 29 10:59:34 2018
 
-This is a temporary script file.
+@author: Loren
 """
-
-a = 1
-a = a+1
-print(a)
-b = 'hello'
-print(b)
-c = b.title()
-print(b)
-print(c)
-d = 'hello'
-e = d.title()
-print(d)
-print(e)
-name = 'Dave'
-f = 'Hello! {0}!'.format(name)
-print(f)
-name = 'Loren'
-f = 'Hello! {0}!'.format(name)
-print(f)
-
-
-age = 5
-age = 'almost there'
-age = 29.5
-age = 'i really don\'t know' 
-print(age)
-
-print(type(2))
-
-print('hello' + 'world')
-print('Joke \n' * 3)
-#print("Chen" + 3) Error because you can't put int and str together in concat. convert int to string.potential fixes below. 
-print("Chen3")
-print("Chen" + "3")
-print("Chen" + str(3))
-print('hello'.upper())
-print()
-print('GOODBYE'.lower())
-print('\n')
-print('the lord of the rings'.title())
-print()
-
-S1 = 'hello' + 'world'
-S2 = 'Joke '*3
-S3 = 5
-result = S1 + S2*10
-print(result)
-print()
-
-s1 = '4'
-s2 = 6
-s3 = 8
-result2 = int(s1) + s2 + s3
-print(result2)
-print()
-
-strExample = 'a,b-b,d,happy'
-print(strExample)
-splitStrExample = strExample.split()
-print(splitStrExample)
-
-
-age = 'fun'
-like = 5
-
-me = 'I am {} and I like {}'.format(age,like)
-print(me)
-age = 6.5
-me = 'I am {} and I like {}'.format(age,like)
-print(me)
+test = '''kjdhfksd    jfhglkjdfhk
+fdghldfghlskdfg
+kgjhlkdfhg'''
+print(test)
