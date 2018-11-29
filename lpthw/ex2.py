918c03e45cf583709f37a54013115d343ca7a572
Loren Bristow <lorendup@gmail.com>
2018-11-29 12:20:25 +0000

    LPTHW ex 2

---
+++ lpthw/ex2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 12:15:10 2018
+
+@author: Loren
+"""
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python. 
+
+print("I could have code like this." # and the comment after is ignored
+)
+# You can also use a comment to 'disable' or comment out a piece of code: 
+# print("This won't run.") 
+print("This will run.")
