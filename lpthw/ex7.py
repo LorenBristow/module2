6f62c81da3168494e5a2132b7343ab5a0cbbd1cd
Loren Bristow <lorendup@gmail.com>
2018-12-02 13:56:30 +0000

    lpthw ex7

---
+++ lpthw/ex7.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+
+print(end1, end ="")
+print(end1 + end2 + end3)
+
+a = [1,2,3,4]
+
+print(end1 + str(a))
+
+print("loren "*3)
