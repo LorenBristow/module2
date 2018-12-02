b81e2034f38e595a8f870d2cd9a489c39e29e5f9
Loren Bristow <lorendup@gmail.com>
2018-12-02 14:07:19 +0000

    lpthw ex 8 and 9

---
+++ lpthw/ex8.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  2 13:57:13 2018
+
+@author: loren
+"""
+
+print(1, 2, 3, 4)
+print("one", "two", "three", "four")
+print(True, False, False, True)
---
+++ lpthw/ex9.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  2 14:01:38 2018
+
+@author: loren
+"""
+days = "\nMon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: " + days)
+print("Here are the monhths: " + months)
+
+print("""
+There's something going on here. 
+With the three double-quotes.
+We'll be able to type as much as we like. 
+EVen 4 lines if we want, or 5, or 6.
+""")
