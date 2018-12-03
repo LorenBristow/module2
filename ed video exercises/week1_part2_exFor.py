67d92670cf569c683e60835f9b40d2eb15e49955
Loren Bristow <lorendup@gmail.com>
2018-12-03 11:00:46 +0000

    ongoing updates

---
+++ ed video exercises/week1_part2_exFor.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  3 10:56:45 2018
+
+@author: loren
+"""
+#EXERCISE 1
+for i in range(2,11,2):
+    print(i)
+print("Goodbye!")
+
+## There are many ways to solve this problem! Here is one:
+#for count in range(11):
+#    if count != 0 and count % 2 == 0:   #% modulus checking if even number
+#        print(count)
+#print("Goodbye!")
+#
+## Here is another:
+#for count in range(2, 12, 2):
+#    print(count)
+#print("Goodbye!")
--- ed video exercises/week1_part2_exWhile.py
+++ ed video exercises/week1_part2_exWhile.py
@@ -18,7 +18,7 @@ widget = 2
 while(widget<=10):
     print(widget)
     widget = widget + 2
-print("Goodbye!")
+print("Goodbye!\n")
 
 ## There are many ways to solve this problem! Here is one:
 #num = 2
@@ -59,9 +59,56 @@ print("Goodbye!")
 print("EXERCISE 2")
 widget = 10
 print("Hello!")
-while widget range(0,10):
+while widget <= 10 and widget > 0:  #unnecessary to have the range - given i  
+                                    #had set widget=10 it will alwys be <=10 i.e. redundant.
     print(widget)
     widget -= 2
     
+## There are always many ways to solve a programming problem. Here is one:
+#print("Hello!")
+#num = 10
+#while num > 0:
+#    print(num)
+#    num -= 2
+#
+## Here is another:
+#num = 11
+#print("Hello!")
+#while num > 1:
+#    num -= 1
+#    if num %2 == 0:
+#        print(num)
+#    
+    
+    
+#EXERCISE 3   #to ponder later - is there potential for an infinite loop here?
+print("EXERCISE 3")
+widget = 0
+end = 6
+while end > 0:
+    widget = widget + end
+    end -= 1
+print(widget)
 
+#This was my first solution which worked but i used a break to prevent inf loop which 
+#was unnec. if i wrote the while expression better. 
+#widget = 0
+#end = 6
+#while end != 0:
+#    if end <0:
+#        break
+#    widget = widget + end
+#    end -= 1
+#print(widget)
+
+
+## Here is one of a few ways to solve this problem:
+#total = 0
+#current = 1
+#while current <= end:
+#    total += current
+#    current += 1
+#
+#print(total)
+    
     
