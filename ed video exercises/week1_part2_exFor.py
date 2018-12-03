bdb55b7f8f02cdd4727f46d5e434db18fe212cdd
Loren Bristow <lorendup@gmail.com>
2018-12-03 11:17:21 +0000

    ongoing updates

--- ed video exercises/week1_part2_exFor.py
+++ ed video exercises/week1_part2_exFor.py
@@ -5,9 +5,10 @@ Created on Mon Dec  3 10:56:45 2018
 @author: loren
 """
 #EXERCISE 1
+print("EXERCISE 1\n")
 for i in range(2,11,2):
     print(i)
-print("Goodbye!")
+print("Goodbye!\n")
 
 ## There are many ways to solve this problem! Here is one:
 #for count in range(11):
@@ -19,3 +20,21 @@ print("Goodbye!")
 #for count in range(2, 12, 2):
 #    print(count)
 #print("Goodbye!")
+
+#EXERCISE 2
+print("EXERCISE 2\n")
+print("Hello!")
+for i in range (10,1,-2):
+    print(i)
+  
+    
+## There are always many ways to solve a programming problem. Here is one:
+#print("Hello!")
+#for num in range(0, 10, 2):
+#    print(10 - num)   ## interesting method :)
+#
+## Here is another:
+#print("Hello!")
+#for num in range(10, 0, -2):
+#    print(num)
+    
