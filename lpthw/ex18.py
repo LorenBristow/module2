e3dcc75615eb26aa847dae54017eb1066e17348d
Loren Bristow <lorendup@gmail.com>
2018-12-04 15:39:30 +0000

    ongoing updates

--- lpthw/ex18.py
+++ lpthw/ex18.py
@@ -4,8 +4,20 @@ Created on Mon Dec  3 15:28:46 2018
 
 @author: loren
 """
-
-def print_two(*args):
-    arg1, arg2 = "'Zed'", "'Shaw'"
+def print_two(*args):  
+    arg1, arg2 = args
     print ("arg1: {}, ".format(arg1), "arg2: {}".format(arg2))
-print_two()    
+
+def print_two_again(arg1, arg2):
+    print ("arg1: {}, ".format(arg1), "arg2: {}".format(arg2))
+  
+def print_one(arg1):
+    print("arg1: {}, ".format(arg1))
+
+def print_none():
+    print("I got nothin'.")
+
+print_two("'Zed'", "'Shaw'")  
+print_two_again("'Zed'", "'Shaw'")  
+print_one("'First!'")
+print_none()
