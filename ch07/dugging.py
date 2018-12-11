9d8d0dec3d2ef06e22bf1bd1430f9289a38a3974
Loren Bristow <lorendup@gmail.com>
2018-12-11 12:14:44 +0000

    ongoing updates

--- ch07/dugging.py
+++ ch07/dugging.py
@@ -1,25 +1,47 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 11 08:10:08 2018
-
-@author: loren
-"""
-
-userInput = input('Please give a number?')
-
-def simpleOperation(userInput):
-    intInput = int(userInput)
-    result = intInput - 2
-    return result 
-
-def nestedOperation(result):
-    result = simpleOperation(userInput)
-    result2 = result * 2
-    return result2
-
-result = simpleOperation(userInput)
-result2 = nestedOperation(result)
-print(result2)
-
-  
-
+ 
+import random
+ 
+ 
+def generate_deck():
+    """Return a list containing the cards in a standard, 52-card deck."""
+    suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
+             "Jack", "Queen", "King", "Ace"]
+    deck = []
+    for s in suits:
+        for r in ranks:
+            name = "%s of %s" % (r, s)
+            deck.append(name)
+    return deck
+ 
+ 
+def draw_random(deck):
+    """Draw a random card from the given deck."""
+    n = len(deck)
+    i = random.randint(0, n)
+    k = draw_card(deck, i)
+    return k
+ 
+ 
+def draw_card(deck, x):
+    """Return card no. x from the deck."""
+    return deck[x - 1]
+ 
+ 
+def test_with_replacement():
+    print("Generating deck")
+    deck = generate_deck()
+ 
+    number = 10
+    print("Drawing", number, "random cards with replacement:")
+    drawn = set()
+    for x in range(number):
+        card = draw_random(deck)
+        print("  You drew", card)
+        drawn.add(card)
+    unique = len(drawn)
+    print("Drew", unique, "different cards")
+ 
+ 
+test_with_replacement()
---
+++ ch08/project.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 11 08:10:05 2018
+
+@author: loren
+"""
+
+
+
+input("Please enter your passcode and hit enter\n")
+print(input)
+passcodeEntered = input
+print(passcodeEntered)
+#checkPasscode()
+#if checkPasscode == True:
+#    print("YOU'RE IN")
+#elif checkPasscode == False:
+#    print("YOU'RE OUT - TRY AGAIN")
+#
+#
+#def checkPasscode(passcodeEntered): # could add option to register later and restrict password
+#    if passcodeEntered == "1234": # dummy passcode, will create separate later
+#        return True
+#    else:
+#        return False # here add additional attempts logic in later version
