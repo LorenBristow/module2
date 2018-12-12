89e640375a166fc21a26ddfc11dd3d23a56e22b1
Loren Bristow <lorendup@gmail.com>
2018-12-12 13:40:32 +0000

    ongoing updates

--- ch05/bank.py
+++ ch05/bank.py
@@ -4,17 +4,28 @@ Created on Wed Dec  5 10:08:21 2018
 
 @author: loren
 """
+
 from bank_functions import *
+loren=Customer('Loren', True, 500)
+# sucker# = Customer("name", bool_hasoverdraft, bal)
+print("WELCOME TO THE BAD BAD BANK\nWHERE GETTING WHAT YOU ASK FOR IS GETTING WHAT YOU PAY FOR...\nand you pay close to nothing.\n" )
+print("Who goes there?!\n")
+name = input()
+name = name.title()
+sucker1 = Customer(name)
+print(sucker1)
 
-Joke = Customer("Joke De Winter", False, 1000) # This is an instance of the class Customer. 
-
+#existing or not
+ # This is an instance of the class Customer. 
 
 print("What would you like to do today?\n1. Make a withdrawl.\n2. Make a deposit.")
 customerActivityChosen = int(input())
 if customerActivityChosen == 1:
     withdrawl_amount = input("How much would you like to withdraw?\n")
-    Joke.withdraw(withdrawl_amount)
-
+    loren.withdraw(withdrawl_amount)
+elif customerActivityChosen == 2:
+    deposit_amount = input("OK Money-bags, how much would you like to deposit?\n")
+    loren.deposit(deposit_amount)
         
 
   
--- ch05/bank_functions.py
+++ ch05/bank_functions.py
@@ -4,6 +4,8 @@ Created on Wed Dec 12 10:15:53 2018
 
 @author: loren
 """
+import random
+
 class Customer():
 #"""Return a Customer object whose name is *name* and starting balance is *balance*."""
     def __init__(self, name,  has_overdraft=False, balance=0.00):
@@ -18,10 +20,22 @@ class Customer():
             print("Withdrawl amount is greater than available balance.\n")
             if self.has_overdraft == False:
                 print("You don't have an overdraft available so you cannot draw this amount. Please withdraw an amount which is equal to or smaller than your available balance of £", self.balance," or apply for an overdraft")
+            #later - add option to change amount requested for withdrawl here or apply for overdraft.
                 does_customer_want_overdraft = input("Would you like to apply for an overdraft? \n  Please respond 'y' for yes or 'n' for no.\n")            
                #need to remember that naming variable of input to something other than input was important to this actually working. 
                 if does_customer_want_overdraft == 'y':
-                    print("Thank you for your application for an overdraft. We will be in touch soon.\n")
+                    print("Thank you for your application for an overdraft. We are considering your application.\n")
+                    print("Wait for it....")
+                    print("Wait for it....")
+                    print("Wait for it....")
+                    print("Wait for it....")
+                    print("Wait for it....")
+                    choice_overdraft = [True, False]
+                    choice_overdraft = random.choice(choice_overdraft)
+                    if choice_overdraft == True:
+                        print("Congratulations, your application is successful and you are now free to weigh yourself down in debt to your heart's delight <3!\n")
+                    elif random.choice == False:
+                        print("Unfortunately your application is denied based on a random 50/50 decision. Goodbye.")
                 else:
                     "Thank you for your response. Please contact us should you change your mind.\n"
                 #raise RuntimeError("Amount greater than available balance.")            
@@ -32,9 +46,15 @@ class Customer():
 
 #"""Return the balance remaining after depositing *amount* dollars."""
     def deposit(self, deposit_amount):
-        self.balance += deposit_amount
-        print("Thank you for your deposit. Your balance has been updated.\n")
-        return self.balance
+        deposit_amount = int(deposit_amount)
+        choice_is_deposit_suspicious = [True, False]
+        choice_is_deposit_suspicious = random.choice(choice_is_deposit_suspicious)
+        if choice_is_deposit_suspicious == False:
+            self.balance += deposit_amount
+            print("Thank you for your deposit. Your balance has been updated.\n")
+            return self.balance
+        elif choice_is_deposit_suspicious == True:
+            print("We have randomly determined that your deposit is SUSPICIOUS. \nPlease got to jail while we drag this matter through court over 5 or so years.\nYou will have the support of a sub-standard lawyer until your funds run dry.")
         
     def __str__(self): #need to add overdraft choice
         return ("CUSTOMER ACCOUNT STATUS:\n  Customer " + self.name + " has a balance of £" + str(self.balance) + ".\n  Overdraft Indicator: " + str(self.has_overdraft) + ".\n\n" )
